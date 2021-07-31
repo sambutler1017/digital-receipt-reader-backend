5e450d6eb5d0f2f0e7c145039d543ee9f3d8020c
Samuel Butler <sambutler1017@icloud.com>
2021-07-30 22:00:30 -0400

    no message

---
+++ src/main/java/com/digital/receipt/DigitalReceiptReaderBackendApplication.java
@@ -0,0 +1,13 @@
+package com.digital.receipt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DigitalReceiptReaderBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DigitalReceiptReaderBackendApplication.class, args);
+	}
+
+}
