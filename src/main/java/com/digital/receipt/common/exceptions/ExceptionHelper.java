cf5ef8d3c0019099cfdd7f3e5f30c25ea3373b1e
Samuel Butler <sambutler1017@icloud.com>
2021-08-24 20:41:50 -0400

    RCSD-25: Adding exception helpr class

---
+++ src/main/java/com/digital/receipt/common/exceptions/ExceptionHelper.java
@@ -0,0 +1,32 @@
+package com.digital.receipt.common.exceptions;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+/**
+ * Exception Helper class for returning response entitys of the errored objects.
+ * 
+ * @author Sam Butler
+ * @since August 24, 2021
+ */
+@ControllerAdvice
+public class ExceptionHelper {
+    private static final Logger LOGGER = LoggerFactory.getLogger(ExceptionHelper.class);
+
+    @ExceptionHandler(InvalidCredentialsException.class)
+    public ResponseEntity<ExceptionError> handleInvalidCredentialsException(Exception ex) {
+        LOGGER.error(ex.getMessage());
+        return new ResponseEntity<ExceptionError>(new ExceptionError(ex.getMessage(), HttpStatus.UNAUTHORIZED),
+                HttpStatus.UNAUTHORIZED);
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ExceptionError> handleException(Exception ex) {
+        LOGGER.error(ex.getMessage());
+        return new ResponseEntity<ExceptionError>(new ExceptionError(ex.getMessage()), HttpStatus.BAD_REQUEST);
+    }
+}
