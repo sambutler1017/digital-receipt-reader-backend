d1eb2316cc7f72d04998ae80a46544efb3e5ac34
Samuel Butler <sambutler1017@icloud.com>
2021-08-28 02:17:32 -0400

    no message

--- src/test/java/com/digital/receipt/service/util/PasswordHashTest.java
+++ src/test/java/com/digital/receipt/service/util/PasswordHashTest.java
@@ -1,17 +1,27 @@
 package com.digital.receipt.service.util;
 
+import com.digital.receipt.app.user.dao.UserDao;
+
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@ActiveProfiles("test-local")
 public class PasswordHashTest {
 
+    @Autowired
+    private UserDao userDao;
+
     @Test
-    public void shouldHashPasswordTest() {
+    public void shouldHashPasswordTest() throws Exception {
+        userDao.getUserById(1);
+        System.out.println(System.getProperty("spring.profiles.active"));
         Assert.assertEquals("Should match", 1, 1);
     }
 }
---
+++ src/test/java/com/digital/receipt/tests/configs/DataSourceTestConfig.java
@@ -0,0 +1,47 @@
+package com.digital.receipt.tests.configs;
+
+import javax.sql.DataSource;
+
+import com.digital.receipt.service.activeProfile.ActiveProfile;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+@Configuration
+@Profile("tmep")
+public class DataSourceTestConfig {
+
+    @Autowired
+    private ActiveProfile activeProfile;
+
+    @Bean
+    public DataSource dataSource() {
+        DriverManagerDataSource ds = new DriverManagerDataSource();
+        ds.setDriverClassName("com.mysql.cj.jdbc.Driver");
+        ds.setUrl(String.format(
+                "jdbc:mysql://databasePI.ddnsfree.com/%s?useSSL=false&useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC",
+                generateTestSchema()));
+        ds.setUsername(activeProfile.isLocalEnvironment() ? "" : System.getenv("MYSQL_USERNAME"));
+        ds.setPassword(activeProfile.isLocalEnvironment() ? "" : System.getenv("MYSQL_PASSWORD"));
+        return ds;
+    }
+
+    @Bean
+    public JdbcTemplate jdbcTemplate() {
+        return new JdbcTemplate(dataSource());
+    }
+
+    /**
+     * Generates a test schema so the DAO test can run on a actual database data
+     * besides production.
+     * 
+     * @return {@link String of the database name}
+     */
+    private String generateTestSchema() {
+        return "TestSchemaName";
+    }
+}
