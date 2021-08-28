79ac8b6d84b72c857e107782cf6f876b447abf63
Samuel Butler <sambutler1017@icloud.com>
2021-08-28 02:41:25 -0400

    no message

--- src/test/java/com/digital/receipt/service/util/PasswordHashTest.java
+++ src/test/java/com/digital/receipt/service/util/PasswordHashTest.java
@@ -5,21 +5,23 @@ import com.digital.receipt.app.user.dao.UserDao;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-@ActiveProfiles("local")
 public class PasswordHashTest {
+    private static final Logger LOGGER = LoggerFactory.getLogger(PasswordHashTest.class);
 
     @Autowired
     private UserDao userDao;
 
     @Test
     public void shouldHashPasswordTest() throws Exception {
+        LOGGER.info("IN TEST");
         userDao.getUserById(1);
         Assert.assertEquals("Should match", 1, 1);
     }
