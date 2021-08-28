54bd345e854842b34b72b6d5c5a86b922b19c527
Samuel Butler <sambutler1017@icloud.com>
2021-08-28 06:08:56 -0400

    no message

--- src/test/java/com/digital/receipt/tests/configs/DataSourceTestConfig.java
+++ src/test/java/com/digital/receipt/tests/configs/DataSourceTestConfig.java
@@ -19,20 +19,9 @@ import org.springframework.jdbc.datasource.DriverManagerDataSource;
 @Configuration
 @Profile("test")
 public class DataSourceTestConfig {
-    private static final String TEST_ENV_PATH = "../digital-receipt-reader-backend/src/test";
-    // Properties prop = new Properties();
-
     @Bean
     public DataSource dataSource() throws SQLException {
         DriverManagerDataSource ds = new DriverManagerDataSource();
-        // if (System.getProperty("APP_ENVIRONMENT") == null) {
-        // initPropertiesFile();
-        // }
-
-        System.out.println("Environment: " + System.getProperty("APP_ENVIRONMENT"));
-        System.out.println("Username: " + System.getProperty("MYSQL_USERNAME"));
-        System.out.println("Password: " + System.getProperty("MYSQL_PASSWORD"));
-
         ds.setDriverClassName("com.mysql.cj.jdbc.Driver");
         ds.setUrl(String.format("jdbc:mysql://databasePI.ddnsfree.com/%s?%s", "receipt_db", getDBParams()));
         ds.setUsername(getUsername());
@@ -45,45 +34,16 @@ public class DataSourceTestConfig {
         return new JdbcTemplate(dataSource());
     }
 
-    /**
-     * Gets the username for the database based on the environment.
-     * 
-     * @return {@link String} of the username to use.
-     */
     private String getUsername() {
         return System.getProperty("APP_ENVIRONMENT") != null ? System.getProperty("MYSQL_USERNAME")
-                : "spring.datasource.username";
+                : "sambutler1017123";
     }
 
-    /**
-     * Gets the password for the database based on the environment.
-     * 
-     * @return {@link String} of the password to use.
-     */
     private String getPassword() {
-        return System.getProperty("APP_ENVIRONMENT") != null ? System.getProperty("MYSQL_PASSWORD")
-                : "spring.datasource.password";
+        return System.getProperty("APP_ENVIRONMENT") != null ? System.getProperty("MYSQL_PASSWORD") : "password123";
     }
 
-    /**
-     * Returns the params to be appended to the db url.
-     * 
-     * @return {@link String} of the db url params.
-     */
     private String getDBParams() {
         return "useSSL=false&useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC";
     }
-
-    /**
-     * Init the properties test file.
-     */
-    // private void initPropertiesFile() {
-    // try (InputStream input = new FileInputStream(
-    // String.format("%s/resources/application-test-local.properties",
-    // TEST_ENV_PATH))) {
-    // prop.load(input);
-    // } catch (IOException io) {
-    // // Continue
-    // }
-    // }
 }
