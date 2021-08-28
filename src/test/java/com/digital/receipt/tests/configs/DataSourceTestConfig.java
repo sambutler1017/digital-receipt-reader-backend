e303f996f873985ea336fe8f897b7d6cde426259
Samuel Butler <sambutler1017@icloud.com>
2021-08-28 04:08:19 -0400

    no message

--- src/test/java/com/digital/receipt/tests/configs/DataSourceTestConfig.java
+++ src/test/java/com/digital/receipt/tests/configs/DataSourceTestConfig.java
@@ -2,9 +2,6 @@ package com.digital.receipt.tests.configs;
 
 import javax.sql.DataSource;
 
-import com.digital.receipt.service.activeProfile.ActiveProfile;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
@@ -18,36 +15,21 @@ import org.springframework.jdbc.datasource.DriverManagerDataSource;
  * @since August 28, 2021
  */
 @Configuration
-@Profile("unknownTest")
+@Profile("test")
 public class DataSourceTestConfig {
-
-    @Autowired
-    private ActiveProfile activeProfile;
-
     @Bean
     public DataSource dataSource() {
         DriverManagerDataSource ds = new DriverManagerDataSource();
         ds.setDriverClassName("com.mysql.cj.jdbc.Driver");
-        ds.setUrl(String.format(
-                "jdbc:mysql://databasePI.ddnsfree.com/%s?useSSL=false&useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC",
-                generateTestSchema()));
-        ds.setUsername(activeProfile.isLocalEnvironment() ? "" : System.getenv("MYSQL_USERNAME"));
-        ds.setPassword(activeProfile.isLocalEnvironment() ? "" : System.getenv("MYSQL_PASSWORD"));
+        ds.setUrl(
+                "jdbc:mysql://databasePI.ddnsfree.com/receipt_db?useSSL=false&useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC");
+        ds.setUsername(System.getenv("MYSQL_USERNAME"));
+        ds.setPassword(System.getenv("MYSQL_PASSWORD"));
         return ds;
     }
 
     @Bean
     public JdbcTemplate jdbcTemplate() {
         return new JdbcTemplate(dataSource());
-    }
-
-    /**
-     * Generates a test schema so the DAO test can run on a actual database data
-     * besides production.
-     * 
-     * @return {@link String of the database name}
-     */
-    private String generateTestSchema() {
-        return "TestSchemaName";
     }
 }
