22c42b461e4b76908d4271cf172dd69f71038cee
Samuel Butler <sambutler1017@icloud.com>
2021-08-28 02:43:18 -0400

    no message

--- src/test/java/com/digital/receipt/service/util/PasswordHashTest.java
+++ src/test/java/com/digital/receipt/service/util/PasswordHashTest.java
@@ -9,10 +9,12 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@ActiveProfiles("test")
 public class PasswordHashTest {
     private static final Logger LOGGER = LoggerFactory.getLogger(PasswordHashTest.class);
 
