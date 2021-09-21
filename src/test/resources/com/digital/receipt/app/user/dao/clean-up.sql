73e082b4fe06b7b85b57d0901d499e9c764050d4
DESKTOP-4QHTB27\sambu <sambutler1017@icloud.com>
2021-09-21 08:48:08 -0400

    no message

--- src/test/resources/com/digital/receipt/app/user/dao/clean-up.sql
+++
@@ -1,7 +0,0 @@
-DELETE FROM user_profile;
-DELETE FROM user_credentials;
-DELETE FROM web_role;
-
-ALTER TABLE user_profile AUTO_INCREMENT = 1;
-ALTER TABLE user_credentials AUTO_INCREMENT = 1;
-ALTER TABLE web_role AUTO_INCREMENT = 1;
