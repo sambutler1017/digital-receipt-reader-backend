65c326d2facc19ab0526b28b309956714dca978f
DESKTOP-4QHTB27\sambu <sambutler1017@icloud.com>
2021-11-18 15:42:20 -0500

    no message

--- src/main/java/com/digital/receipt/app/receipt/mapper/ReceiptMapper.java
+++ src/main/java/com/digital/receipt/app/receipt/mapper/ReceiptMapper.java
@@ -2,6 +2,7 @@ package com.digital.receipt.app.receipt.mapper;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.Date;
 
 import com.digital.receipt.app.receipt.client.domain.Receipt;
 
@@ -22,7 +23,7 @@ public class ReceiptMapper implements RowMapper<Receipt> {
         receipt.setId(rs.getInt("id"));
         receipt.setUserId(rs.getInt("user_id"));
         receipt.setFilePublicId(rs.getString("file_public_id"));
-        receipt.setInsertDate(rs.getDate("insert_date_utc"));
+        receipt.setInsertDate(new Date(rs.getDate("insert_date_utc").getTime()));
         receipt.setLocation(rs.getString("location"));
         receipt.setLabel(rs.getString("label"));
 
